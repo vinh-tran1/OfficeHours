3b1a4512ea67fa1cf64da7a1c03905960f6aa746
VinhTran <vinhtrann101@gmail.com>
2024-03-21 00:23:25 -0400

    small change to run.py

--- backend/run.py
+++ backend/run.py
@@ -8,26 +8,9 @@ import sys
 import os
 from app import app
 
-
-# def get_port():
-#     """ parse the command line arguments for port
-#         store argument as variable
-#     """
-    
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('port', metavar='port', type=int,
-#                     help='the port at which the server should listen')
-
-#     args = parser.parse_args()
-#     port =  args.port
-
-#     return port
-
 def main():
     """ main function to start flask app
     """
-
-    # port = get_port()
     port = int(os.getenv('PORT', 80))
 
     try:
