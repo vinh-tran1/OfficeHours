22b587d5ae04073a2c265f55d6e2f9ba61cfaaab
VinhTran <vinhtrann101@gmail.com>
2024-05-01 16:29:12 -0400

    faq

--- frontend/src/pages/FAQ/FAQ.jsx
+++ frontend/src/pages/FAQ/FAQ.jsx
@@ -7,30 +7,30 @@ const FAQ = () => {
   const data1 = [
     {
       question: "What is Office(H)ours?",
-      answer: "Answer Here"
+      answer: "Office hours management app for maximizing user experience."
     },
     {
       question: "What does the (H) mean?",
-      answer: "Answer Here"
+      answer: "This is a reference to asymptotic notation i.e. O(N)."
     },
     {
       question: "Who runs Office(H)ours?",
-      answer: "Answer Here"
+      answer: "Created by Nikhil, Haroon, and Vinh for a CS final project."
     },
     {
       question: "What is the history of Office(H)ours?",
-      answer: "Answer Here"
+      answer: "3 students. 1 goal. Better office hours."
     }
   ];
 
   const data2 = [
     {
       question: "Who can use O(H)?",
-      answer: "Answer Here"
+      answer: "Students! TAs! Professors!"
     },
     {
       question: "How do I use O(H)?",
-      answer: "Answer Here"
+      answer: "Add classes and view on calendar. Easy and simple!"
     }
   ];
 
